1775fb1da0d01b460b3db791ad6dd9c9614313be
mehedi-hasannnn <mhasan2999@gmail.com>
2024-10-23 10:34:29 +0600

    visited country dynamic feature added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -8,13 +8,20 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
 
+
+    const passWithParams = () =>{
+        handleVisitedCountry(country);
+    }
+
     return (
-        <div className='country'>
-            <h3>Name: {name.common}</h3>
+        <div  className={`country ${visited ? 'visited-style': 'non-visited'}`}>
+            <h3 style={{color: visited ? 'purple': 'black'}}>Name: {name.common}</h3>
             <img src={flags.png} alt="Country flags" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Mark as Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ?'Visited':'Going'}</button>
             {visited ? 'I have visited this country' : 'I want to visit this country'}
         </div>
